96089583dcff6157651dca960f4fe937bc2a46f6
Nathan Faucett <nathanfaucett@gmail.com>
2015-05-02 19:50:38 -0500

    expose some util functions

--- src/index.js
+++ src/index.js
@@ -12,6 +12,15 @@ virt.View = View;
 virt.createView = View.create;
 virt.createFactory = View.createFactory;
 
+virt.consts = require("./transaction/consts");
+
+virt.getChildKey = require("./utils/get_child_key");
+
 virt.registerNativeComponent = require("./utils/register_native_component");
+
+virt.traverseAncestors = require("./utils/traverse_ancestors");
+virt.traverseDescendants = require("./utils/traverse_descendants");
+virt.traverseTwoPhase = require("./utils/traverse_two_phase");
+
 virt.context = require("./context");
 virt.owner = require("./owner");
